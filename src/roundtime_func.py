060a9b4f9f04cee956a31f61877e1326d2041e8e
cloutzie <cloutzie@owlpost.nl>
2023-04-02 15:33:19 -0400

    added callable hook to sheet

--- src/roundtime_func.py
+++ src/roundtime_func.py
@@ -2,20 +2,13 @@ import pandas as pd
 import current_func
 from datetime import datetime
 import time
+import sheet_hook as sh
 
 
 
-sheet_id = "1RzqqheHKMmwCbEM_gSTUep_S4hFu4ILMz__o0tjYPF0"
-sheet_name = "FEBRUARY-2023"
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-col_names = ["turns", "celine", "chocolat", "fergus", "lenny", "lednas"]
-col_nums = [0,1,3,5,7,9]
 
-df = pd.read_csv(url, usecols= col_nums, names = col_names, skiprows=1)
-turns = range(1,df.count()['turns']+1)
-df['turns'] = turns
-df = df.dropna()[df['turns'] <= current_func.current('round')]
-df
+
+
 
 
 def roundtime(round):
